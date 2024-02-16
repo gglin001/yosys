ec51fd25d97316646531daf0f086fc2e0c941732
Allen Guo <guosonglin001@gmail.com>
2024-02-17 00:35:13 +0800

    wip

--- t.sh
+++ t.sh
@@ -1,7 +1,11 @@
 git clone https://github.com/YosysHQ/abc
 
+# edit `Makefile.conf`
+
 # asume conda env `bear` has bear installed
 $CONDA_PREFIX/../bear/bin/bear -- make -j10
+
+# make install
 
 # patchelf
 # micromamba install -y patchelf
@@ -12,4 +16,12 @@ install_name_tool -add_rpath $CONDA_PREFIX/lib yosys
 
 pushd examples/aiger
 bash demo.sh
+popd
+
+pushd examples/cxx-api
+# no `make install`
+yosys-config --exec --cxx -o demomain --cxxflags --ldflags demomain.cc -I../../ -L../../ -lyosys -lstdc++
+# # need `make install`
+# yosys-config --exec --cxx -o demomain --cxxflags --ldflags demomain.cc -I../../ -L../../build/lib/yosys -lyosys -lstdc++
+install_name_tool -add_rpath $CONDA_PREFIX/lib demomain
 popd
